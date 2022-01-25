a95ca9ca8e8f20f18ba1c2d92a3cc8ac83d7a8c9
Keaton <keaton.arneson@gmail.com>
2022-01-25 15:52:48 -0600

    Moved yelp api functionality to useResults hook

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,27 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const searchApi = async searchTerm => {
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: 'grand forks',
-        },
-      });
-      setResults(response.data.businesses);
-    } catch (err) {
-      setErrorMessage('Something went wrong');
-    }
-  };
+  const [searchApi, results, errorMessage] = useResults();
 
   return (
     <View>
@@ -32,6 +17,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
+      <ResultsList />
+      <ResultsList />
+      <ResultsList />
     </View>
   );
 };
